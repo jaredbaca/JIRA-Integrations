29bcabedc4b8037ac45d51b290e7ef5f461bb02f
Jared Baca <110132943+jaredjames90@users.noreply.github.com>
2024-02-06 11:29:20 -0500

    testing commit

---
+++ credentials.js
@@ -0,0 +1,11 @@
+module.exports = {
+    atlassian: {
+        username: 'jaredbacamusic@gmail.com',
+        api_key: 'ATATT3xFfGF0dMH0byCdQW6hk0QabyWbSy2y2Se2CLmMowi1T1dIhHBK-InlmxrdhMZl8joWhg-Sn7Q3h6BobnIHXbCcBcoiClP3T3pvUaTzfxLRQp0l3ImDeYYWKyz3low6wMNQrtrEgSWyTTARh1FUPHmZX6WPPPocqOgAFAtd5b_Vfz5Ro54=254BCBF2',
+    },
+
+    calendly: {
+        api_token: 'eyJraWQiOiIxY2UxZTEzNjE3ZGNmNzY2YjNjZWJjY2Y4ZGM1YmFmYThhNjVlNjg0MDIzZjdjMzJiZTgzNDliMjM4MDEzNWI0IiwidHlwIjoiUEFUIiwiYWxnIjoiRVMyNTYifQ.eyJpc3MiOiJodHRwczovL2F1dGguY2FsZW5kbHkuY29tIiwiaWF0IjoxNzA0NjgwMDg5LCJqdGkiOiJjMzBiOTIwZi03YjM2LTQ4MWUtOTQ3Yi0yNGJkOThkMTE0ZGQiLCJ1c2VyX3V1aWQiOiIxNDBjYmQ2My1hZjU3LTQzZDktYTVmZC1kYTZiMzdlN2MzODgifQ.F8LqB5vEGFDqOtfnA600iQYMst5kIXuSgzRjdOWsNm_eet83UwBe7SdkORWa46-EZZnL8PdYedXPq-aM3Ztm3Q'
+    }
+    
+}
